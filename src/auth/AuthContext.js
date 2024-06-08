9155d09a4051748f5d8cf0d61c327b85d11a238a
Aghata Sophia <aghatasophia@gmail.com>
2024-06-08 16:30:47 -0400

    Create the AuthContext and AuthContextProvider to control the logged user

---
+++ src/auth/AuthContext.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebase-config.js";
+
+function useAuth() {
+    const [currentUser, setCurrentUser] = useState({});
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setCurrentUser(user);
+        });
+        return () => unsubscribe();
+    }, []);
+
+    return { currentUser };
+}
+
+export const AuthContext = React.createContext();
+
+export function AuthContextProvider({ children }) {
+    const { currentUser } = useAuth();
+
+    return (
+        <AuthContext.Provider value={{ currentUser }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}

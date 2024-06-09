7f4f52ebc6d8703e47bf894b111bc305a9d8dd53
dianatobe96 <dianana996@gmail.com>
2024-06-08 20:52:52 -0400

    create the footer and the footer css

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Footer.css'
+
+export default function Footer() {
+    return (
+        <footer className='footer'>
+             <h4 className='footer-text'>Copyright © All rights reserved TravelP.</h4>
+        </footer>
+        
+    )
+}

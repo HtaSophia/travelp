f87e61bef552fcb2d35bbe9cca98d957b05d31ac
Aghata Sophia <aghatasophia@gmail.com>
2024-06-08 20:15:08 -0400

    Update firebase path

--- src/auth/AuthContext.js
+++ src/auth/AuthContext.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../firebase-config.js";
+import { auth } from "../firebase/firebase-config.js";
 
 function useAuth() {
     const [currentUser, setCurrentUser] = useState({});

90df1cf12033669b0a34f8d1c73b9657f8678baa
dianatobe96 <dianana996@gmail.com>
2024-06-09 14:01:55 -0400

    I add the props of the button

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
+import Button from './components/shared/Button/Button';
 
 function App() {
   return (<>
   <Navbar username='name'/>
   <Footer/>
+  <Button text='button'/>
   
   </>);
 }
--- src/components/shared/Button/Button.js
+++ src/components/shared/Button/Button.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Button.css'
+import PropTypes from 'prop-types'
 
-export default function Button() {
+Button.propTypes = {
+    text: PropTypes.string.isRequired,
+    color: PropTypes.string.isRequired, 
+    icon: PropTypes.string,                 
+    onClick: PropTypes.func,                
+}
+
+function Button(props) {
+    const { text, color, icon, onClick } = props;
+
+    // Use a state for the color style if needed
+    const [colorStyle] = useState(color);
+
     return (
-        <div>Button</div>
+        <button 
+            type='button' 
+            className='button' 
+            style={{ backgroundColor: colorStyle }} 
+            onClick={onClick}
+        >
+            {icon && <img src={icon} alt="icon" className='button-icon' />}
+            {text}
+        </button>
     )
 }
+
+export default Button;

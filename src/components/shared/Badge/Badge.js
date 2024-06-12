7c493de0db588a7a6669befe0ff462ea3b3a8296
Aghata Sophia <aghatasophia@gmail.com>
2024-06-12 12:24:15 -0400

    Fix the Badge color css

--- src/components/shared/Badge/Badge.js
+++ src/components/shared/Badge/Badge.js
@@ -5,7 +5,7 @@ function Badge(props) {
     const { tooltipText = '', color = 'text-bg-primary', icon } = props;
 
     return (
-        <div className={`badge${color}`}>
+        <div className={`badge ${color}`}>
             {icon && <i className={`bi bi-${icon}`}></i>}
             <span className="tooltip-text">{tooltipText}</span>
         </div>

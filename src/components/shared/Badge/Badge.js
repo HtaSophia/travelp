9d684fe3809345c06ab07332e8449c9c1970b5af
Aghata Sophia <aghatasophia@gmail.com>
2024-06-12 12:23:11 -0400

    Update Badge style

--- src/components/shared/Badge/Badge.js
+++ src/components/shared/Badge/Badge.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import './Badge.css';
-import PropTypes from 'prop-types';
-;
-
 
 function Badge(props) {
-    const { tooltipText = '', color = 'primary', icon } = props;
+    const { tooltipText = '', color = 'text-bg-primary', icon } = props;
 
     return (
-
-        <span className={` badge text-bg-${color}`} >
+        <div className={`badge${color}`}>
             {icon && <i className={`bi bi-${icon}`}></i>}
-            <span className="tooltiptext">{tooltipText}</span>
-        </span>
-
-
-
-
+            <span className="tooltip-text">{tooltipText}</span>
+        </div>
     )
 };
 

5f5b8d8158ef9a5df64237a45c19dd97aea5eb4a
Samira Salimian <iamsaly2212@gmail.com>
2024-06-13 21:14:14 -0400

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React, { useContext } from 'react';
 import { Routes, Route, Navigate } from "react-router-dom";
 import { AuthContext } from './auth/AuthContext.js';
-
+import Badge from './components/shared/Badge/Badge.js';
 import Navbar from './components/Navbar/Navbar.js';
 import Footer from './components/Footer/Footer.js';
 import Singin from './components/Singin/Singin.js';
@@ -20,6 +20,13 @@ function App() {
     if (!currentUser) return <Navigate to="/login" />
     return children;
   };
+
+  // Example array of markers
+  const markers = [
+    { position: { lat: 40.7128, lng: -74.006 }, title: 'Marker 1' },
+    { position: { lat: 40.7259, lng: -73.9805 }, title: 'Marker 2' },
+    // Add more markers as needed
+  ];
 
   const handleCardClick = () => {
     alert('Card clicked!');

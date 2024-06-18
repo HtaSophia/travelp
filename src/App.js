d7789d3ef3e978684ae6f87e12bfbc9bf9ef30fe
Aghata Sophia <aghatasophia@gmail.com>
2024-06-18 19:41:37 -0400

    Add TravelForm to the App component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
-import './App.css';
-import notFoundImg from './assets/icons/404.svg';
-import React from 'react';
+import "./App.css";
+import notFoundImg from "./assets/icons/404.svg";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-import Authentication from './auth/components/Authentication.js';
-import ProtectedRoute from './auth/components/ProtectedRoutes.js';
-import SingIn from './pages/SignIn/SignIn.js';
-import SignUp from './pages/SignUp/SignUp.js';
+import Authentication from "./auth/components/Authentication.js";
+import ProtectedRoute from "./auth/components/ProtectedRoutes.js";
+import SingIn from "./pages/SignIn/SignIn.js";
+import SignUp from "./pages/SignUp/SignUp.js";
+import TravelForm from "./pages/TravelForm/TravelForm.js";
+import Map from "./components/Map/Map.js";
 
 function App() {
     return (
         <Routes>
-            <Route element={<ProtectedRoute />}>
-                <Route path="/travels" index element={<h1>Travels</h1>} />
-                <Route path="/travel-details" element={<h1>Travel Details</h1>} />
-                <Route path="/travel" element={<h1>Edit Travel</h1>} />
+            <Route path="/" element={<ProtectedRoute />}>
+                <Route path="/travels" index element={<Map />} />
+                <Route
+                    path="/travel-details/:travelId"
+                    element={<h1>Travel Details</h1>}
+                />
+                <Route path="/travel/:travelId?" element={<TravelForm />} />
             </Route>
 
             <Route element={<Authentication />}>
-                <Route path='sign-up' element={<SignUp />}></Route>
-                <Route path='sign-in' element={<SingIn />}></Route>
+                <Route path="/sign-up" element={<SignUp />}></Route>
+                <Route path="/sign-in" element={<SingIn />}></Route>
             </Route>
 
-            <Route path='*' element={
-                <div className="text-center">
-                    <img src={notFoundImg} alt="draw of a florets with a 404 number in the middle" />
-                </div>
-            }></Route>
+            <Route
+                path="*"
+                element={
+                    <div className="text-center">
+                        <img
+                            src={notFoundImg}
+                            alt="draw of a florets with a 404 number in the middle"
+                        />
+                    </div>
+                }
+            ></Route>
         </Routes>
-    )
+    );
 }
 
 export default App;
